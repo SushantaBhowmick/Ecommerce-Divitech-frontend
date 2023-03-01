ef4493db3b3998da985229bbdcd0c53276d99692
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-03-01 23:03:08 +0530

    Create More than one section and components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Marquee from 'react-fast-marquee'
 import BlogCard from '../components/BlogCard'
+import ProductCard from '../components/ProductCard'
 
 const Home = () => {
   return (
@@ -210,7 +211,21 @@ const Home = () => {
         </div>
       </section>
       
-      <div className="blog-wrapper py-5 home-wrapper-2 ">
+      <section className="blog-wrapper py-5 home-wrapper-2 ">
+        <div className="container-xxl ">
+          <div className="row ">
+            <div className="col-12">
+              <h3 className="sextion-heading">Featured Collections</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </section>
+      
+      <section className="blog-wrapper py-5 home-wrapper-2 ">
         <div className="container-xxl ">
           <div className="row ">
             <div className="col-12">
@@ -222,9 +237,11 @@ const Home = () => {
             <BlogCard />
           </div>
         </div>
-      </div>
+      </section>
     </>
   )
 }
 
 export default Home
+
+//2.30.00
